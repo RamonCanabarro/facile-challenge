e26fa99c7e0bc5180a08a815caa28590b2cd33e2
Ramon Canabarro <ramon.canabarro@widup.com.br>
2021-12-17 20:40:18 -0300

    route get test

--- src/server.js
+++ src/server.js
@@ -14,7 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(encriptRoutes)
-
+app.get("/", function (req, res) {
+  res.status(200).json({success: "FACILE CHALLENGE!!!!"})
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
